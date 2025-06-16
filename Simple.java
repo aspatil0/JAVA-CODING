20c1c2789bdb826e5e1d77169b6c4fe70057cd08
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-06-17 01:48:27 +0530

    sum of 2 number

---
+++ Simple.java
@@ -0,0 +1,16 @@
+public class Simple {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Hello, World!");
+
+        // Declare two integers
+        int a = 5;
+        int b = 10;
+
+        // Calculate the sum
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
